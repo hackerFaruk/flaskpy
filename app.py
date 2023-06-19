204565694e4c22b5398e44ea9e7ceaf2d9203d67
hackerFaruk <onuralparslan97@gmail.com>
2023-06-19 20:19:19 +0300

    added database

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    #girdiler için id oluştur
+    id=db.Column(db.Integer, primary_key=True)
+    #girdi içeriğini kaydet
+    content = db.Column(db.String(200), nullable=False)
+    # oluşturulma tarini al
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
